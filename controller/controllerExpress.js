61d607d74896d0d0a91db4fac3bdbee8a8c9d8f9
ullmans <shilo.ullman@gmail.com>
2021-05-18 20:03:56 +0300

    added a controller folder

--- controller/controllerExpress.js
+++ controller/controllerExpress.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const path = require('path');
-const model = require(path.join(__dirname, '../model/anomalyDetectionModel'))
+const model = require(path.join(__dirname, './model/anomalyDetectionModel'))
 
 const app = express()
 app.use(express.urlencoded({
@@ -12,7 +12,7 @@ app.use(fileUpload())
 app.use(express.static("."))
 
 app.get('/', (req,res)=> {
-    res.sendFile(path.join(__dirname, "../view/index.html"))
+    res.sendFile(path.join(__dirname, "./view/index.html"))
 })
 
 app.post("/detect", (req,res)=>{
